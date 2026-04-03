260ba7f80d086b9ebe9145380d2933257fe01dbd
singampalliprakash <jnanaprakash2002@gmail.com>
2026-04-03 21:05:07 +0530

    Debug: Open CORS for mobile registration

--- server/server.js
+++ server/server.js
@@ -17,14 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: function (origin, callback) {
-    const allowedOrigins = ['http://localhost', 'capacitor://localhost', 'http://localhost:5173', 'http://localhost:5000'];
-    if (!origin || allowedOrigins.some(o => origin.startsWith(o)) || origin.includes('vercel.app')) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: '*',
   credentials: true,
 }));
 app.use(express.json());
